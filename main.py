d3b9dc29366a9ea28e247a965505e8cc8dad6fb4
SarahEssaam <sarahessam29@hotmail.com>
2017-12-17 23:16:13 +0200

    Main thread working

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from FrontEnd import frontEnd
+from FrontEnd import frontEnd , MyWidget
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QThread
+
+
+class Main(QThread):
+    def __init(self):
+        QThread.__init__(self)
+
+    def run(self):
+        app = QtWidgets.QApplication(sys.argv)
+        dialog = QtWidgets.QMainWindow()
+        self.test_1 = frontEnd(dialog)
+        dialog.show()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QMainWindow()
-    test_1 = frontEnd(dialog)
-    dialog.show()
-    sys.exit(app.exec_())
+
+    Integration= MyWidget()
+    M = Main()
+    M.run()
+    #Sniffer().run(MyWidget)
